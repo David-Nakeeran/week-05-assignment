ea3a90b9761b891a050fd5d49aac6fde7b9a1ed7
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-05-28 21:37:04 +0100

    Added error handling

--- client/src/confirmation.js
+++ client/src/confirmation.js
@@ -1,20 +1,26 @@
 //to ensure the DOM is loaded and ready as soon as the page is opened ==> utilized the "DOMContentLoaded"
 window.addEventListener("DOMContentLoaded", async () => {
-    const response = await fetch("http://localhost:8080/newBooking/latest"); //link to the query to get latest addition to the table 
+  try {
+    const response = await fetch("http://localhost:8080/newBooking/latest"); //link to the query to get latest addition to the table
+
     const bookings = await response.json();
-  
+
+    if (!response.ok) {
+      throw new Error(bookings.error.message);
+    }
+
     const summary = document.getElementById("booking-summary");
     summary.innerHTML = ""; // Clear old content
-  
-    
-      const div = document.createElement("div");
-      div.innerHTML = `
+
+    const div = document.createElement("div");
+    div.innerHTML = `
         <h3>Hotel: ${bookings.hotel_name}</h3>
         <p>Customer: ${bookings.customer_name}</p>
         <p>Check-in: ${bookings.check_in}</p>
         <p>Check-out: ${bookings.check_out}</p>
       `;
-      summary.appendChild(div);
-    
-  });
-  
+    summary.appendChild(div);
+  } catch (error) {
+    console.error("Booking confirmation failed:", error.message);
+  }
+});
